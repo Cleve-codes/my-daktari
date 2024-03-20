e7d6065f68142b83c972bd764b5a8dad0234a9a6
Cleve-codes <clevemomanyi@gmail.com>
2024-03-20 08:07:21 +0300

    changes

--- backend/Controllers/reviewController.js
+++ backend/Controllers/reviewController.js
@@ -1,38 +1,35 @@
 import Review from "../models/ReviewSchema.js";
 import Doctor from "../models/DoctorSchema.js";
 
-// Get all reviews
+//get all reviews
 export const getAllReviews = async (req, res) => {
   try {
     const reviews = await Review.find({});
-    res.status(200).json({ success: true, message: 'Succesfull', data: reviews });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Successful", data: reviews });
   } catch (error) {
     res.status(404).json({ success: false, message: "Not found" });
   }
 };
 
-// create a review
+//create review
 export const createReview = async (req, res) => {
-
-  if (!req.userId) {
-    return res.status(401).json({ success: false, message: "Unauthorized" });
-  }
-
   if (!req.body.doctor) req.body.doctor = req.params.doctorId;
   if (!req.body.user) req.body.user = req.userId;
 
   const newReview = new Review(req.body);
-
   try {
-
     const savedReview = await newReview.save();
     await Doctor.findByIdAndUpdate(req.body.doctor, {
       $push: { reviews: savedReview._id },
     });
 
-    res.status(200).json({ success: true, message: "Review created", data: savedReview });
+    res
+      .status(200)
+      .json({ success: true, message: "Review submitted", data: savedReview });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Not found" });
+    res.status(500).json({ success: false, message: error.message });
   }
-
 };
