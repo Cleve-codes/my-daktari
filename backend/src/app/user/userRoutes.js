ce6099e4fda7d61590eb985379ae9718d64751a9
Cleve-codes <clevemomanyi@gmail.com>
2024-03-25 19:27:50 +0300

    dashboard

--- backend/src/app/user/userRoutes.js
+++ backend/src/app/user/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/:id', authVerify.authenticate, authVerify.restrict(['patient']), userController.getSingleUser)
 router.get('/', authVerify.authenticate, authVerify.restrict(['admin']), userController.getAllUser)
 router.get('/profile/me', authVerify.authenticate, authVerify.restrict(['patient']), userController.getUserProfile)
-router.get('/appointment/my-appointment', authVerify.authenticate, authVerify.restrict(['patient']), userController.getMyAppointment)
+router.get('/appointments/my-appointments', authVerify.authenticate, authVerify.restrict(['patient']), userController.getMyAppointment)
 router.put('/:id', authVerify.authenticate, authVerify.restrict(['patient']), userController.updateUser)
 router.delete('/:id', authVerify.authenticate, authVerify.restrict(['patient']), userController.deleteUser)
 
