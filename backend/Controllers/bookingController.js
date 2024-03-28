73e09016d93fb8cc0a0d1507562cb93e7fe9d239
Cleve-codes <clevemomanyi@gmail.com>
2024-03-28 22:31:07 +0300

    stripeDone

--- backend/Controllers/bookingController.js
+++ backend/Controllers/bookingController.js
@@ -16,7 +16,7 @@ export const getCheckoutSession = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}checkout-success`,
+      success_url: `https://my-daktari.vercel.app/checkout-success`,
       cancel_url: `${req.protocol}://${req.get('host')}/doctors/${doctor.id}`,
       customer_email:user.email,
       client_reference_id:req.params.doctorId,
