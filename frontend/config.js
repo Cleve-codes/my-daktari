bb4b39d17fb015afbe1ee632445fe84913dbed7d
Cleve-codes <clevemomanyi@gmail.com>
2025-03-19 21:40:15 +0300

    [Update] environment update

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,9 @@
 // export const BASE_URL = `https://my-daktari.onrender.com
-export const BASE_URL = `http://localhost:8000`
+// export const BASE_URL = `http://localhost:8000`
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? `https://my-daktari.onrender.com`
+    : `http://localhost:8000`;
 
 export const token = localStorage.getItem('token')
 
